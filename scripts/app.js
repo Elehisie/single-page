c4a8ffb1c563944333a888bc723a90b593102fdb
Camila Paschini <elehisie@gmail.com>
2017-09-07 13:44:19 +0200

    polishing the menu

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,21 @@
   var controlMenu = function() {
     var toggler = document.getElementsByClassName('toggler');
     var slideMenu = document.getElementById('slideMenu');
-
+    var defaultNav = document.getElementById('defaultNav');
+    
+   
+    
     for ( i=0; i<toggler.length; i++) {
       toggler[i].addEventListener('click', function() {
         if (slideMenu.style.display == 'flex') {
           // this.style.display = 'flex';
           slideMenu.style.display= 'none';
+           defaultNav.style.display = 'flex';
         }
         else {
           // this.style.display = 'none';
           slideMenu.style.display = 'flex';
+           defaultNav.style.display = 'none';
         }
       });
     }
